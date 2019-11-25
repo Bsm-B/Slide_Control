b87189b4937e5bf2e84f6c4a3f745fcbf71c18da
unknown <bsmbousselmi@gmail.com>
2019-11-24 16:47:07 -0800

    ADD Client

---
+++ ClientPC/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstPorts = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 82)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(612, 144)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "PORT"
+        '
+        'lstPorts
+        '
+        Me.lstPorts.FormattingEnabled = True
+        Me.lstPorts.Location = New System.Drawing.Point(43, 32)
+        Me.lstPorts.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.lstPorts.Name = "lstPorts"
+        Me.lstPorts.Size = New System.Drawing.Size(612, 24)
+        Me.lstPorts.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(712, 310)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstPorts)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "SlideControl"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstPorts As ComboBox
+End Class
---
+++ ClientPC/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+Imports System
+Imports System.IO.Ports
+Imports System.Runtime.InteropServices
+Public Class Form1
+    Dim WithEvents SerialPort As New IO.Ports.SerialPort
+    Dim Readdata As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If lstPorts.SelectedIndex <> -1 Then
+            Try
+                If SerialPort.IsOpen Then
+                    SerialPort.Close()
+                    Button1.Text = "Connect"
+
+                Else
+                    SerialPort.PortName = lstPorts.SelectedItem.ToString
+                    SerialPort.BaudRate = 9600
+                    SerialPort.DataBits = 8
+                    SerialPort.Parity = Parity.None
+                    SerialPort.StopBits = StopBits.One
+                    SerialPort.Handshake = Handshake.None
+                    SerialPort.Encoding = System.Text.Encoding.Default
+                    SerialPort.RtsEnable = True
+                    SerialPort.Open()
+                    Button1.Text = "Disconnect"
+                    AddHandler SerialPort.DataReceived, AddressOf DataReceivedHandler
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Else
+            MsgBox("Please choose a serial port", vbInformation, "Serial Port")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetSerialPortNames()
+    End Sub
+
+    Private Sub DataReceivedHandler(sender As Object, e As SerialDataReceivedEventArgs)
+
+        Dim K As String = SerialPort.ReadLine()
+        If (Asc(K) = 97) Then
+
+            SendKeys.SendWait("{LEFT}")
+            SendKeys.Flush()
+
+        End If
+
+        If (Asc(K) = 98) Then
+
+            SendKeys.SendWait("{RIGHT}")
+            SendKeys.Flush()
+
+        End If
+        K = ""
+        SerialPort.DiscardInBuffer()
+
+    End Sub
+    Sub GetSerialPortNames()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            lstPorts.Items.Add(sp)
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
